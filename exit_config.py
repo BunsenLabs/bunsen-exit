ef7cc30d14bd1cac57772c4e6aa6758e8eb41808
William Bradley <webradley9929@gmail.com>
2018-02-07 14:20:55 -0600

    Began the function that does validatin tests on the configured theme. Added comments to ~/.config/bunsen-exit/bl-exitrc that explain the useage of the theme entries.

--- exit_config.py
+++ exit_config.py
@@ -79,12 +79,6 @@ class Config(object):
 					key = item[ 0 ]
 					val = item[ 1 ]
 					theme[ key ] = val
-			#theme_list = config_file.items(theme)
-			#for item in theme_list:
-			#	for x in range(0, len(item)):
-			#		key = item[ 0 ]
-			#		val = item[ 1 ]
-			#		theme_entries[ key ] = val
 		else:
 			exit_log.warn("No config file found. Using defaults.")
 			button_values = {'Cancel':'show', 'Logout':'show', 'Suspend':'show',
@@ -115,5 +109,133 @@ class Config(object):
 
 	def validate_theme_entries(self, theme, theme_entries):
 		# Ensure that all theme_entries conform to some sort of sensible defaults
+		# Theme entries will default to the bunsen-small theme with a grey style.
+		# In order to emulate legacy versions of this app.
+
+		# Validate name
+		try:
+			if theme_entries['name'] == "" or theme_entries['name'] == None:
+				theme_entries['name'] = "Uknown"
+				exit_log.warn("Theme entry for name was empty. Setting to Unknown")
+		except:
+			exit_log.warn("<key error>: name not found in config file. Setting name to Unknown")
+			theme_entries['name'] = "Unknown"
+
+		# Validate author
+		try:
+			if theme_entries['author'] == "" or ['window_width_adjustment'] == None:
+				theme_entries['author'] = "Uknown"
+				exit_log.warn("Theme entry for author was empty. Setting to Unknown")
+		except:
+			exit_log.warn("<key error>: author not found in config file. Setting author to Unknown")
+			theme_entries['author'] = "Unknown"
+
+		# Validate window_width_adjustment.
+		try:
+			if theme_entries['window_width_adjustment'] == "" or ['window_width_adjustment'] == None:
+				theme_entries['window_width_adjustment'] = 0.50
+				exit_log.warn("Theme entry for window_width_adjustment was empty. Setting to 0.50")
+			try:
+				temp = double(theme_entries['window_width_adjustment'])
+			except:
+				exit_log.warn["window_width_adjustment is not a float. Defaulting to 0.50"]
+				theme_entries['window_width_adjustment'] = 0.50
+		except:
+			exit_log.warn("<key error>: window_width_adjustment not found in config file.") 
+			exit_log.warn("Setting window_width_adjustment to 0.50.")
+			theme_entries['window_width_adjustment'] = 0.50
+
+		# Validate dialog_height. Early versions defaults to 130
+		try:
+			if theme_entries['dialog_height'] == "" or theme_entries['dialog_height'] == None:
+				theme_entries['dialog_height'] = 130
+				exit_log.warn("Theme entry for dialog_height was empty. Setting to 130")
+			try:
+				temp = int(theme_entries['dialog_height'])
+			except:
+				exit_log.warn("dialog_height is not an int. Defaulting to 130")
+				theme_entries['dialog_height'] = 130
+		except:
+			exit_log.warn("<key error>: dialog_height not found in config file.")
+			exit_log.warn("Setting dialog_height to 130.")
+			theme_entries['dialog_height'] = 130
+
+		# Validate button_height. For early version of this program,
+		# dialog_height was the button_height, so button_height will default to 130
+		try:
+			if theme_entries['button_height'] == "" or theme_entries['button_height'] == None:
+				theme_entries['button_height'] = 130
+				exit_log.warn("Theme entry for button_height was empty. Setting to 130")
+			try:
+				temp = int(theme_entries['button_height')
+			except:
+				exit_log.warn("button_height is not an int. Defaulting to 130")
+				theme_entries['button_height'] = 130
+		except:
+			exit_log.warn("<key error>: button_height not found in config file.")
+			exit_log.warn("Setting button_height to 130.")
+
+		# Validate inner_border. Early versions did not use this setting,
+		# so inner_border defaults to 0
+		try:
+			if theme_entries['inner_border'] == "" or theme_entries['inner_border'] == None:
+				theme_entries['inner_border'] = 0
+				exit_log.warn("Theme entry for inner_border was empty. Setting to 0.")
+			try:
+				temp = int(theme_entries['inner_border']
+			except:
+				exit_log.warn("inner_border is not an int. Defaulting to 0")
+				theme_entries['inner_border'] = 0
+		except:
+			exit_log.warn("<key error>: inner_border not found in config file.")
+			exit_log.warn("Setting inner_border to 0")
+			theme_entries['inner_border'] = 0
+
+		# Validate sleep_delay. Dafaults to 0.001
+		try:
+			if theme_entries['sleep_delay'] == "" or theme_entries['sleep_delay'] == None:
+				theme_entries['sleep_delay'] = 0.001
+				exit_log.warn("Theme entry for sleep_delay was empty. Setting to 0.001")
+			try:
+				temp = theme_entries['sleep_delay'] = float(theme_entries['sleep_delay']
+			except:
+				exit_log.warn("sleep_delay is not a float. Defaulting to 0.001")
+				theme_entries['sleep_delay'] = 0.001
+		except:
+			exit_log.warn("<key error>: sleep_delay not found in config file.")
+			exit_log.warn("Setting sleep_delay to 0.001")
+			theme_entries['sleep_delay'] = 0.001
+		
+		# Validate overall_opacity
+		try:
+			if theme_entries['overall_opacity'] == "" or theme_entries['overall_opacity'] = None:
+				theme_entries['overall_opacity'] = 80
+				exit_log.warn("overall_opacity was empty. Setting to 80")
+			try:
+				temp = int(theme_entries['overall_opacity'])
+			except:
+				exit_log.warn("overall opacity is not an int. Defaulting to 80")
+				theme_entries['overall_opacity'] = 80
+		except:
+			exit_log.warn("<key error>: overall_opacity not found in config file.")
+			exit_log.warn("Setting overall_opacity to 80")
+			theme_entries['overall_opacity'] = 80
+		
+		# Validate button spacing. Prior versions did not use this.
+		# Defaults to 0.
+		try:
+			if theme_entries['button_spacing'] == "" or theme_entries['button_spacing'] = None:
+				theme_entries['button_spacing'] = 80
+				exit_log.warn("button_spacing was empty. Setting to 0.")
+			try:
+				temp = int(theme_entries['button_spacing']
+			except:
+				exit_log.warn("button_spacing is not an int. Defaulting to 0.")
+				theme_entries['button_spacing'] = 0
+		except:
+			exit_log.warn("<key error>: button_spacing not found in config file.")
+			exit_log.warn("Setting button_spacing to 0")
+			theme_entries['button_spacing'] = 0
+		
 		
 		return
